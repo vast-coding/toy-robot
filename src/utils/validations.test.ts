45c07d97fcbab88347d380b03be27a999c17ab15
vasilly <vasko.traikovski@gmail.com>
2020-12-02 11:49:50 +1100

    refactor: update tests

--- src/utils/validations.test.ts
+++ src/utils/validations.test.ts
@@ -7,24 +7,24 @@ import {
 
 describe('validations', () => {
   test('checkIsValidCoordinates should give true for valid coordinates', () => {
-    expect(checkIsValidCoordinates('1', '4')).toStrictEqual(true)
+    expect(checkIsValidCoordinates('1', '4')).toBeTruthy()
   })
 
   test('checkIsValidCoordinates should give false for invalid coordinates', () => {
-    expect(checkIsValidCoordinates('10', '40')).toStrictEqual(false)
+    expect(checkIsValidCoordinates('10', '40')).toBeFalsy()
   })
 
   test('checkIsValidFace should give true for valid face', () => {
-    expect(checkIsValidFace('NORTH')).toStrictEqual(true)
+    expect(checkIsValidFace('NORTH')).toBeTruthy()
   })
   test('checkIsValidFace should give false for invalid face', () => {
-    expect(checkIsValidFace('north')).toStrictEqual(false)
+    expect(checkIsValidFace('north')).toBeFalsy()
   })
 
   test('checkIsValidInstruction should give true for valid instruction', () => {
-    expect(checkIsValidInstruction('MOVE')).toStrictEqual(true)
+    expect(checkIsValidInstruction('MOVE')).toBeTruthy()
   })
   test('checkIsValidInstruction should give false for invalid instruction', () => {
-    expect(checkIsValidInstruction('move')).toStrictEqual(false)
+    expect(checkIsValidInstruction('move')).toBeFalsy()
   })
 })
